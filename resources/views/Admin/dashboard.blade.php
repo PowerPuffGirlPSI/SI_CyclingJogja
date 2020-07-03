dde2e02492f5bfb74332c801bbfe1d7994fbfd9e
Avilia22 <18523174@students.uii.ac.id>
2020-07-03 09:32:54 +0700

    baru dashboard

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -9,9 +9,12 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Cycling Jogja Admin - Dashboard</title>
+  <title>Cycling Jogja Admin - Laporan Sepeda</title>
 
-  <!-- Custom fonts for this template -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> 
+ <!-- Custom fonts for this template -->
   <link href="/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
@@ -20,7 +23,7 @@
 
   <!-- Custom styles for this page -->
   <link href="/admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-  
+  <script src="/admin/vendor/chart.js/Chart.js"></script>
 </head>
 
 <body id="page-top">
@@ -43,10 +46,11 @@
       <hr class="sidebar-divider my-0">
   
       <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="/admin/dashboard">
           <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
+          <span>Dashboard</span>
+        </a>
       </li>
   
       <!-- Divider -->
@@ -58,7 +62,7 @@
       </div>
   
       <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-table"></i>
           <span>Daftar Tabel</span>
@@ -70,8 +74,7 @@
             <a class="collapse-item" href="/admin/tabel_event">Event</a>
             <a class="collapse-item" href="/admin/tabel_tiket">Pemesanan Tiket</a>
             <a class="collapse-item" href="/admin/tabel_penyewa">Penyewa Sepeda</a>
-            <a class="collapse-item" href="/admin/tabel_sepeda">Sepeda</a>
-            
+            <a class="collapse-item" href="/admin/tabel_sepeda">Sepeda</a>  
           </div>
         </div>
       </li>
@@ -82,6 +85,16 @@
           <span>Laporan</span></a>
       </li>
           
+      <li class="nav-item active">
+        <a class="nav-link"  href="/admin/laporan_peserta">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Peserta Event</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link"  href="/admin/laporan_penyewa">
+          <i class="fas fa-fw fa-chart-area"></i>
+          <span>Penyewa Sepeda</span></a>
+      </li>
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
